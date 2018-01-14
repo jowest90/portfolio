2f16efe71e31d4fae6cc2dc099f1c9a36ab0fc2d
jowest90 <jowest1990@gmail.com>
2018-01-13 22:34:28 -0600

    auth
    create user auth

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::prefix('/')->group(function(){
+    Route::get('/', 'HomeController@index');
+    Route::get('/home', 'HomeController@index');
+  });
