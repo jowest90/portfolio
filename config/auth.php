cf4e7b532132cb06df514569306b731bdc0dd0d0
jowest90 <jowest1990@gmail.com>
2018-02-19 17:17:13 -0600

    all ruinsed

--- config/auth.php
+++ config/auth.php
@@ -45,15 +45,6 @@ return [
             'driver' => 'token',
             'provider' => 'users',
         ],
-        'admin' => [
-            'driver' => 'session',
-            'provider' => 'admins',
-        ],
-        'admin-api' => [
-            'driver' => 'token',
-            'provider' => 'admins',
-        ],
-
     ],
 
     /*
@@ -77,10 +68,6 @@ return [
         'users' => [
             'driver' => 'eloquent',
             'model' => App\User::class,
-        ],
-        'admins' => [
-            'driver' => 'eloquent',
-            'model' => App\Admin::class,
         ],
 
         // 'users' => [
@@ -109,11 +96,6 @@ return [
             'provider' => 'users',
             'table' => 'password_resets',
             'expire' => 60,
-        ],
-        'admins' => [
-            'provider' => 'admins',
-            'table' => 'password_resets',
-            'expire' => 15,
         ],
     ],
 
