f921b0ec2e191808db1d3386b436ceb6b99c6bbb
jowest90 <jowest1990@gmail.com>
2019-08-02 13:42:30 -0500

    indent lines

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,12 +18,12 @@
                     Here, you can view some of my latest as well as previous forms of work that I've done in the past. All porjects can be viewed on the tabs above the page.
                     Enjoy your stay!
 
-                    -John W. West III
+                    <p>-John W. West III</p>
 
-                    Contact info:
-                    Email- jowest1990@gmail.com
-                    Skype- jowest1990@outlook.com
-                    Phone #- 618 402 2314
+                    <p>Contact info:
+                    <p>Email- jowest1990@gmail.com</p>
+                    <p>Skype- jowest1990@outlook.com</p>
+                    <p>Phone #- 618 402 2314</p>
                 </div>
             </div>
         </div>
