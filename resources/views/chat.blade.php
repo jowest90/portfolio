be01925e705b45464b55d836f9e60d97cf9a8606
John West <jowest1990@gmail.com>
2022-06-04 18:55:44 -0500

    fix auth

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -2,23 +2,22 @@
 
 @section('content')
 <div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Chat Room</div>
-
-                <ul class="list-group">
-                @foreach($users as $chatuser)
-                    <li class="list-group-item" id="{{ $chatuser->id }}" value="{{ $chatuser->name }}">{{ $chatuser->name }}</li>
-                @endforeach
-              </ul>
-
-              <chat-log :messages="messages"></chat-log>
-              <div class="col-md-8 col-md-2">
-              <chat-composer v-on:messagesent="addMessage"></chat-composer>
-              </div>
-            </div>
-        </div>
-    </div>
+  <div class="col-md-2">
+    <ul class="list-group">
+    @foreach($users as $chatuser)
+        <li class="list-group-item" id="{{ $chatuser->id }}" value="{{ $chatuser->name }}">{{ $chatuser->name }}</li>
+    @endforeach
+  </ul>
+  </div>
+  <div class="col-md-8 col-md-2">
+      <div class="panel panel-default">
+  <div class="panel-heading">Chat Room</div>
 </div>
+</div>
+<chat-log :messages="messages"></chat-log>
+<div class="col-md-8 col-md-2">
+<chat-composer v-on:messagesent="addMessage"></chat-composer>
+</div>
+</div>
+
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -61,41 +61,35 @@ Route::prefix('/')->group(function(){
   /*
   Movie Appl;ication
   */
+
   // routes for cashier
+  Route::get('/cashier', 'Cashier\CashierController@index');
+  Route::get('/cashier/getMenuByCategory/{category_id}', 'Cashier\CashierController@getMenuByCategory');
+  Route::get('/cashier/getTable', 'Cashier\CashierController@getTables');
+  Route::get('/cashier/getSaleDetailsByTable/{table_id}', 'Cashier\CashierController@getSaleDetailsByTable');
 
-  Route::middleware(['auth'])->group(function(){
+  Route::post('/cashier/orderFood', 'Cashier\CashierController@orderFood');
+  Route::post('/cashier/deleteSaleDetail', 'Cashier\CashierController@deleteSaleDetail');
 
-      // routes for cashier
-      Route::get('/cashier', 'Cashier\CashierController@index');
-      Route::get('/cashier/getMenuByCategory/{category_id}', 'Cashier\CashierController@getMenuByCategory');
-      Route::get('/cashier/getTable', 'Cashier\CashierController@getTables');
-      Route::get('/cashier/getSaleDetailsByTable/{table_id}', 'Cashier\CashierController@getSaleDetailsByTable');
+  Route::post('/cashier/confirmOrderStatus', 'Cashier\CashierController@confirmOrderStatus');
+  Route::post('/cashier/savePayment', 'Cashier\CashierController@savePayment');
+  Route::get('/cashier/showReceipt/{saleID}', 'Cashier\CashierController@showReceipt');
 
-      Route::post('/cashier/orderFood', 'Cashier\CashierController@orderFood');
-      Route::post('/cashier/deleteSaleDetail', 'Cashier\CashierController@deleteSaleDetail');
-
-      Route::post('/cashier/confirmOrderStatus', 'Cashier\CashierController@confirmOrderStatus');
-      Route::post('/cashier/savePayment', 'Cashier\CashierController@savePayment');
-      Route::get('/cashier/showReceipt/{saleID}', 'Cashier\CashierController@showReceipt');
+  Route::get('/management', function(){
+      return view('movie.management.index');
   });
+  //routes for management
+  Route::resource('management/category','Management\CategoryController');
+  Route::resource('management/menu','Management\MenuController');
+  Route::resource('management/table','Management\tableController');
+  Route::resource('management/user','Management\UserController');
+  //routes for report
 
-  Route::middleware(['auth', 'VerifyAdmin'])->group(function(){
-      Route::get('/management', function(){
-          return view('movie.management.index');
-      });
-      //routes for management
-      Route::resource('management/category','Management\CategoryController');
-      Route::resource('management/menu','Management\MenuController');
-      Route::resource('management/table','Management\tableController');
-      Route::resource('management/user','Management\UserController');
-      //routes for report
+  Route::get('/report', 'Report\ReportController@index');
+  Route::get('/report/show', 'Report\ReportController@show');
 
-      Route::get('/report', 'Report\ReportController@index');
-      Route::get('/report/show', 'Report\ReportController@show');
-
-      // Export to excel
-      Route::get('/report/show/export', 'Report\ReportController@export');
-  });
+  // Export to excel
+  Route::get('/report/show/export', 'Report\ReportController@export');
 /*
 --------------------------------------------------------------------------------
 */
