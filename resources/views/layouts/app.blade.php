a7f395f71d99bd4c6fcde46bc95a74c5df833b11
John West <jowest1990@gmail.com>
2022-06-05 10:41:29 -0500

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,19 +24,19 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="'/'">
+                <a class="navbar-brand" href="/">
                     Profile
                 </a>
-                <a class="navbar-brand" href="'/description'">
+                <a class="navbar-brand" href="/description">
                     App description
                 </a>
                 <a class="navbar-brand" href="/chat">
                     </i>Chat room
                 </a>
-                <a class="navbar-brand" href="'/customer'">
+                <a class="navbar-brand" href="/customer">
                     </i>Customer
                 </a>
-                <a class="navbar-brand" href="'/employee'">
+                <a class="navbar-brand" href="/employee">
                     </i>Employee
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
