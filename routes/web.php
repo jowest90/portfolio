c70f94ee8ea448276cbbf9f8018243f790918185
John West <jowest1990@gmail.com>
2022-06-05 09:03:46 -0500

    register fix

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@
 
 Route::get('/', 'HomeController@index');
 
-Auth::routes(['register' => false, 'reset' => false]);
+// Auth::routes(['register' => false, 'reset' => false]);
 
 Route::get('/home', 'HomeController@index')->name('home');
 
@@ -24,10 +24,10 @@ Route::middleware(['auth'])->group(function(){
     Route::get('/cashier/getMenuByCategory/{category_id}', 'Cashier\CashierController@getMenuByCategory');
     Route::get('/cashier/getTable', 'Cashier\CashierController@getTables');
     Route::get('/cashier/getSaleDetailsByTable/{table_id}', 'Cashier\CashierController@getSaleDetailsByTable');
-    
+
     Route::post('/cashier/orderFood', 'Cashier\CashierController@orderFood');
     Route::post('/cashier/deleteSaleDetail', 'Cashier\CashierController@deleteSaleDetail');
-    
+
     Route::post('/cashier/confirmOrderStatus', 'Cashier\CashierController@confirmOrderStatus');
     Route::post('/cashier/savePayment', 'Cashier\CashierController@savePayment');
     Route::get('/cashier/showReceipt/{saleID}', 'Cashier\CashierController@showReceipt');
@@ -43,11 +43,10 @@ Route::middleware(['auth', 'VerifyAdmin'])->group(function(){
     Route::resource('management/table','Management\tableController');
     Route::resource('management/user','Management\UserController');
     //routes for report
-    
+
     Route::get('/report', 'Report\ReportController@index');
     Route::get('/report/show', 'Report\ReportController@show');
-    
+
     // Export to excel
     Route::get('/report/show/export', 'Report\ReportController@export');
 });
-
