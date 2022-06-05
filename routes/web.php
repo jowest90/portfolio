d48393d0e22471553b5bca7f5b1cde492f4cbd93
John West <jowest1990@gmail.com>
2022-06-05 15:45:55 -0500

    reg fix 3.0

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@
 
 Route::get('/', 'HomeController@index');
 
-Auth::routes(['register' => false, 'reset' => false]);
+// Auth::routes(['register' => false, 'reset' => false]);
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::post('/chat','ChatController@sendMessage');
