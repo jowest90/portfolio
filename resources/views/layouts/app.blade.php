65f8b9e750d2db248db7deb51ede12fb1ce1037c
John West <jowest1990@gmail.com>
2022-06-05 10:01:54 -0500

    add titles

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,19 +25,19 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="'/'">
-                    <i class="fas fa-utensils"></i>{{ config('Profile', 'Laravel') }}
+                    <i class="fas fa-utensils"></i>Profile
                 </a>
                 <a class="navbar-brand" href="'/description'">
-                    <i class="fas fa-utensils"></i>{{ config('app.name', 'Laravel') }}
+                    <i class="fas fa-utensils"></i>App description
                 </a>
                 <a class="navbar-brand" href="/chat">
-                    <i class="fas fa-utensils"></i>{{ config('Chat room', 'Laravel') }}
+                    <i class="fas fa-utensils"></i>Chat room
                 </a>
                 <a class="navbar-brand" href="'/customer'">
-                    <i class="fas fa-utensils"></i>{{ config('Customer.name', 'Laravel') }}
+                    <i class="fas fa-utensils"></i>Customer
                 </a>
                 <a class="navbar-brand" href="'/employee'">
-                    <i class="fas fa-utensils"></i>{{ config('Employee.name', 'Laravel') }}
+                    <i class="fas fa-utensils"></i>Employee
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
