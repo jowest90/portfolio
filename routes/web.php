537129f8e85b4f4f7b72d2e951db41a460bc13c7
John West <jowest1990@gmail.com>
2022-06-05 12:18:58 -0500

    chat install

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,20 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return redirect('login');
 });
+
+Route::post('getFriends', 'HomeController@getFriends');
+Route::post('/session/create', 'SessionController@create');
+Route::post('/session/{session}/chats', 'ChatController@chats');
+Route::post('/session/{session}/read', 'ChatController@read');
+Route::post('/session/{session}/clear', 'ChatController@clear');
+Route::post('/session/{session}/block', 'BlockController@block');
+Route::post('/session/{session}/unblock', 'BlockController@unblock');
+Route::post('/send/{session}', 'ChatController@send');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/cha
