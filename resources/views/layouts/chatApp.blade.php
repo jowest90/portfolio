04b4675a8e0ff61f19b927946cdc3cb837e43532
John West <jowest1990@gmail.com>
2022-06-05 14:12:06 -0500

    foodApp

--- resources/views/layouts/chatApp.blade.php
+++ resources/views/layouts/chatApp.blade.php
@@ -3,29 +3,31 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <script>
+        window.auth = {!! auth()->user() !!}
+    </script>
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
 
-    <!-- Fonts -->
+     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/home') }}">
-                    <i class="fas fa-utensils"></i>{{ config('app.name', 'Laravel') }}
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    {{ config('app.name', 'Laravel') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,11 @@
 
 
 
-Route::get('/', function () {
-    return redirect('login');
-});
+// Route::get('/', function () {
+//     return redirect('login');
+// });
+Route::get('/', 'HomeController@index');
+Auth::routes(['register' => false, 'reset' => false]);
 
 Route::post('getFriends', 'HomeController@getFriends');
 Route::post('/session/create', 'SessionController@create');
@@ -21,3 +23,37 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/chat', 'HomeController@chat')->name('chat');
 
 Auth::routes();
+
+Route::middleware(['auth'])->group(function(){
+
+    // routes for cashier
+    Route::get('/cashier', 'Cashier\CashierController@index');
+    Route::get('/cashier/getMenuByCategory/{category_id}', 'Cashier\CashierController@getMenuByCategory');
+    Route::get('/cashier/getTable', 'Cashier\CashierController@getTables');
+    Route::get('/cashier/getSaleDetailsByTable/{table_id}', 'Cashier\CashierController@getSaleDetailsByTable');
+
+    Route::post('/cashier/orderFood', 'Cashier\CashierController@orderFood');
+    Route::post('/cashier/deleteSaleDetail', 'Cashier\CashierController@deleteSaleDetail');
+
+    Route::post('/cashier/confirmOrderStatus', 'Cashier\CashierController@confirmOrderStatus');
+    Route::post('/cashier/savePayment', 'Cashier\CashierController@savePayment');
+    Route::get('/cashier/showReceipt/{saleID}', 'Cashier\CashierController@showReceipt');
+});
+
+Route::middleware(['auth', 'VerifyAdmin'])->group(function(){
+    Route::get('/management', function(){
+        return view('management.index');
+    });
+    //routes for management
+    Route::resource('management/category','Management\CategoryController');
+    Route::resource('management/menu','Management\MenuController');
+    Route::resource('management/table','Management\tableController');
+    Route::resource('management/user','Management\UserController');
+    //routes for report
+
+    Route::get('/report', 'Report\ReportController@index');
+    Route::get('/report/show', 'Report\ReportController@show');
+
+    // Export to excel
+    Route::get('/report/show/export', 'Report\ReportController@export');
+});
