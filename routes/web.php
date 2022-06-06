bde9993b9eb6f28331c859d10f73e32abaec6b32
John West <jowest1990@gmail.com>
2022-06-06 00:01:58 -0500

    get post category

--- routes/web.php
+++ routes/web.php
@@ -29,6 +29,11 @@ Route::group(['middleware' => ['auth']], function() {
         });
         //routes for management
         Route::get('/management/category',[App\Http\Controllers\Management\CategoryController::class, 'index']);
+        Route::get('/management/category/create',[App\Http\Controllers\Management\CategoryController::class, 'create']);
+        Route::post('/management/category',[App\Http\Controllers\Management\CategoryController::class, 'store']);
+        Route::get('/management/category/{{$category->id}}/edit',[App\Http\Controllers\Management\CategoryController::class, 'edit']);
+        Route::post('/management/category/{{$category->id}}',[App\Http\Controllers\Management\CategoryController::class, 'update']);
+        Route::post('/management/category/{{$category->id}}',[App\Http\Controllers\Management\CategoryController::class, 'destroy']);
         Route::get('/management/menu',[App\Http\Controllers\Management\MenuController::class, 'index']);
         Route::get('/management/table',[App\Http\Controllers\Management\tableController::class, 'index']);
         Route::get('/management/user',[App\Http\Controllers\Management\UserController::class, 'index']);
