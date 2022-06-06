a1f6967d3edeb6744cc949deb728c3629ecc791e
John West <jowest1990@gmail.com>
2022-06-05 20:37:10 -0500

    chat app test

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,39 +5,17 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Main Functions</div>
+                <div class="card-header">{{ __('Dashboard') }}</div>
 
                 <div class="card-body">
                     @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
                     @endif
 
-                    <div class="row text-center">
-                        @if(Auth::user()->checkAdmin())
-                        <div class="col-sm-4">
-                            <a href="/management">
-                                <h4>Managment</h4>
-                                <img width="50px" src="{{asset('images/management.svg')}}"/>
-                            </a>
-                        </div>
-                        @endif
-                        <div class="col-sm-4">
-                            <a href="/cashier">
-                                <h4>Cashier</h4>
-                                <img width="50px" src="{{asset('images/cashier.svg')}}"/>
-                            </a>
-                        </div>
-                        @if(Auth::user()->checkAdmin())
-                        <div class="col-sm-4">
-                            <a href="/report">
-                                <h4>Report</h4>
-                                <img width="50px" src="{{asset('images/report.svg')}}"/>
-                            </a>
-                        </div>
-                        @endif
-                    </div>
+                    {{ __('You are logged in!') }}
+                    <a href="{{ url('/chat') }}">Chat</a>
                 </div>
             </div>
         </div>
