d16652c49991d11f81019b3023ed28c8380a1463
John West <jowest1990@gmail.com>
2022-06-05 21:29:50 -0500

    webb fix

--- routes/web.php
+++ routes/web.php
@@ -13,26 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-<?php
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::get('/', 'HomeController@index');
 
-Auth::routes(['register' => true, 'reset' => true]);
+Auth::routes(['register' => false, 'reset' => false]);
 
 Route::get('/home', 'HomeController@index')->name('home');
 
@@ -69,8 +56,6 @@ Route::middleware(['auth', 'VerifyAdmin'])->group(function(){
     // Export to excel
     Route::get('/report/show/export', 'Report\ReportController@export');
 });
-
-
 
 Auth::routes();
 
