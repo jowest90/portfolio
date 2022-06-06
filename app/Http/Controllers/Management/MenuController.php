d1f7a03751893108bf64e14cf59532251cb4f71b
John West <jowest1990@gmail.com>
2022-06-05 23:52:03 -0500

    controller fix

--- app/Http/Controllers/Management/MenuController.php
+++ app/Http/Controllers/Management/MenuController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Management;
 use App\Models\Menu;
 use App\Models\Category;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class MenuController extends Controller
 {
