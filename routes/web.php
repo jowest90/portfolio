8586a0e2254549a42dd96cebff15a79977d9f492
John West <jowest1990@gmail.com>
2022-06-05 23:12:14 -0500

    auth check

--- routes/web.php
+++ routes/web.php
@@ -18,24 +18,26 @@ Route::get('/', function () {
 });
 
 Auth::routes();
+Route::group(['middleware' => ['auth']], function() {
+    // your routes
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::get('/chat', [App\Http\Controllers\ChatsController::class, 'index']);
+    Route::get('/messages', [App\Http\Controllers\ChatsController::class, 'fetchMessages']);
+    Route::post('/messages', [App\Http\Controllers\ChatsController::class, 'sendMessage']);
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/chat', [App\Http\Controllers\ChatsController::class, 'index']);
-Route::get('/messages', [App\Http\Controllers\ChatsController::class, 'fetchMessages']);
-Route::post('/messages', [App\Http\Controllers\ChatsController::class, 'sendMessage']);
+    Route::get('/management', function(){
+            return view('management.index');
+        });
+        //routes for management
+        Route::resource('management/category','Management\CategoryController');
+        Route::resource('management/menu','Management\MenuController');
+        Route::resource('management/table','Management\tableController');
+        Route::resource('management/user','Management\UserController');
+        //routes for report
 
-Route::get('/management', function(){
-        return view('management.index');
-    });
-    //routes for management
-    Route::resource('management/category','Management\CategoryController');
-    Route::resource('management/menu','Management\MenuController');
-    Route::resource('management/table','Management\tableController');
-    Route::resource('management/user','Management\UserController');
-    //routes for report
+        Route::get('/report', 'Report\ReportController@index');
+        Route::get('/report/show', 'Report\ReportController@show');
 
-    Route::get('/report', 'Report\ReportController@index');
-    Route::get('/report/show', 'Report\ReportController@show');
-
-    // Export to excel
-    Route::get('/report/show/export', 'Report\ReportController@export');
+        // Export to excel
+        Route::get('/report/show/export', 'Report\ReportController@export');
+});
