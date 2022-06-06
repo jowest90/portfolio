03a5e795eff83d69a4c2d99decf96e84db14fbfb
John West <jowest1990@gmail.com>
2022-06-05 23:06:01 -0500

    minagement initial

--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,19 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Route::get('/chat', [App\Http\Controllers\ChatsController::class, 'index']);
 Route::get('/messages', [App\Http\Controllers\ChatsController::class, 'fetchMessages']);
 Route::post('/messages', [App\Http\Controllers\ChatsController::class, 'sendMessage']);
+
+Route::get('/management', function(){
+        return view('management.index');
+    });
+    //routes for management
+    Route::resource('management/category','Management\CategoryController');
+    Route::resource('management/menu','Management\MenuController');
+    Route::resource('management/table','Management\tableController');
+    Route::resource('management/user','Management\UserController');
+    //routes for report
+
+    Route::get('/report', 'Report\ReportController@index');
+    Route::get('/report/show', 'Report\ReportController@show');
+
+    // Export to excel
+    Route::get('/report/show/export', 'Report\ReportController@export');
