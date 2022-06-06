c03467077c4b5df9648af3a786fa5d6f78d5b4e9
John West <jowest1990@gmail.com>
2022-06-06 05:24:56 -0500

    fix sale detail

--- database/migrations/2022_06_06_083419_create_sale_details_table.php
+++ database/migrations/2022_06_06_083419_create_sale_details_table.php
@@ -13,10 +13,16 @@ class CreateSaleDetailsTable extends Migration
      */
     public function up()
     {
-        Schema::create('sale_details', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
+      Schema::create('sale_details', function (Blueprint $table) {
+          $table->bigIncrements('id');
+          $table->integer('sale_id');
+          $table->integer('menu_id');
+          $table->string('menu_name');
+          $table->integer('menu_price');
+          $table->integer('quantity');
+          $table->string('status')->default('noConfirm'); // confirm or noconfirm
+          $table->timestamps();
+      });
     }
 
     /**
