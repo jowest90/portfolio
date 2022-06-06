d32d2f0ad1e13d851a949ec65082221d48d587b2
John West <jowest1990@gmail.com>
2022-06-06 13:07:10 -0500

    Update ChatsController.php

--- app/Http/Controllers/ChatsController.php
+++ app/Http/Controllers/ChatsController.php
@@ -21,7 +21,7 @@ class ChatsController extends Controller
      */
     public function index()
     {
-        return view('chat');
+        return view('chat')>withUsers($users);
     }
 
     /**
