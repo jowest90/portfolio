2f21e8265adc5847f26e48d2f35128a77f4f52da
John West <jowest1990@gmail.com>
2022-06-06 04:08:11 -0500

    center icons

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,30 +16,31 @@
 
                     {{ __('You are logged in!') }}
                     <a href="{{ url('/chat') }}">Chat</a>
-                    <!-- management -->
+                    <!-- Food App -->
                     <div class="row text-center">
+                        <!-- management -->
                       <div class="col-sm-4">
                           <a href="/management">
                               <h4>Managment</h4>
                               <img width="50px" src="{{asset('images/management.svg')}}"/>
                           </a>
                       </div>
+                      <!-- cashier -->
+                      <div class="col-sm-4">
+                              <a href="/cashier">
+                                  <h4>Cashier</h4>
+                                  <img width="50px" src="{{asset('images/cashier.svg')}}"/>
+                              </a>
+                          </div>
+                      <!-- report -->
+                      <div class="col-sm-4">
+                              <a href="/report">
+                                  <h4>Report</h4>
+                                  <img width="50px" src="{{asset('images/report.svg')}}"/>
+                              </a>
+                          </div>
+                      <!-- Modal -->
                     </div>
-                    <!-- cashier -->
-                    <div class="col-sm-4">
-                            <a href="/cashier">
-                                <h4>Cashier</h4>
-                                <img width="50px" src="{{asset('images/cashier.svg')}}"/>
-                            </a>
-                        </div>
-                    <!-- report -->
-                    <div class="col-sm-4">
-                            <a href="/report">
-                                <h4>Report</h4>
-                                <img width="50px" src="{{asset('images/report.svg')}}"/>
-                            </a>
-                        </div>
-                    <!-- Modal -->
                 </div>
             </div>
         </div>
