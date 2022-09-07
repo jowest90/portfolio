84021780ae5e0271e87464e352b325295fc92024
John West <jowest1990@gmail.com>
2022-09-07 01:45:30 -0500

    seeder test

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,5 +15,17 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $sql = public_path('website.sql');
+
+        $db = [
+            'username' => env('DB_USERNAME'),
+            'password' => env('DB_PASSWORD'),
+            'host' => env('DB_HOST'),
+            'database' => env('DB_DATABASE')
+        ];
+
+        exec("mysql --user={$db['username']} --password={$db['password']} --host={$db['host']} --database {$db['database']} < $sql");
+
+        \Log::info('SQL Import Done');
     }
 }
